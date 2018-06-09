15b4558be8593724cab17065e3d670e72a4efcb5
Polacek Patrik <patrik.polacek@tieto.com>
2018-06-09 09:56:06 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Test<Number> test = new <Double>Test<Number>(6.23);
+
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,9 @@
+package com.company;
+
+
+public class Test<T> {
+
+    public <U extends T> Test(U k) {
+
+    }
+}
